2da0cbebd95e548b36ca3ea72a848bfea1331ab4
shengshampoo <shengzh@tutanota.com>
2025-09-07 23:34:53 +0800

    Create build-static-lib.sh

---
+++ build-static-lib.sh
@@ -0,0 +1,19 @@
+
+#! /bin/bash
+#build some static lib only for qemu 9.2
+WORKSPACE=/tmp/workspace
+mkdir -p $WORKSPACE
+
+HOST_OS_RAW=$(uname -s)
+HOST_ARCH_RAW=$(uname -m)
+
+HOST_OS=${HOST_OS_RAW,,}
+HOST_ARCH=${HOST_ARCH_RAW,,}
+
+# xq
+cd $WORKSPACE
+git clone https://github.com/MiSawa/xq.git 
+cd xq 
+RUSTFLAGS="-C target-feature=+crt-static -C linker=clang -C strip=symbols -C opt-level=s" 
+CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER=clang 
+cargo build --target ${HOST_ARCH}-chimera-linux-musl --release
