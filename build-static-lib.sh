3d5786421633d711a6b9c9372ce7746220b1e2a2
shengshampoo <shengzh@tutanota.com>
2025-09-08 14:40:28 +0800

    Update build-static-lib.sh

--- build-static-lib.sh
+++ build-static-lib.sh
@@ -11,61 +11,61 @@ HOST_OS=$(uname -s)
 HOST_ARCH=$(uname -m)
 
 # bandwhich
+pkgs="bandwhich"
 cd $WORKSPACE
 git clone https://github.com/imsnif/bandwhich.git
-cd bandwhich
+cd ${pkgs}
 RUSTFLAGS="-C target-feature=+crt-static -C linker=clang -C strip=symbols -C opt-level=s"
 cargo build --target ${HOST_ARCH}-chimera-linux-musl --release
 cd ./target/${HOST_ARCH}-chimera-linux-musl/release/
-XZ_OPT=-e9 tar vcJf ./bandwhich.tar.xz bandwhich
-mv ./bandwhich.tar.xz /work/artifact/
+XZ_OPT=-e9 tar vcJf ./${pkgs}.tar.xz -C /work/artifact/ ${pkgs}
 
 # xq
+pkgs="xq"
 cd $WORKSPACE
 git clone https://github.com/MiSawa/xq.git
-cd xq
+cd ${pkgs}
 RUSTFLAGS="-C target-feature=+crt-static -C linker=clang -C strip=symbols -C opt-level=s"
 cargo build --target ${HOST_ARCH}-chimera-linux-musl --release
 cd ./target/${HOST_ARCH}-chimera-linux-musl/release/
-XZ_OPT=-e9 tar vcJf ./xq.tar.xz xq
-mv ./xq.tar.xz /work/artifact/
+XZ_OPT=-e9 tar vcJf ./${pkgs}.tar.xz -C /work/artifact/ ${pkgs}
 
 # jless
+pkgs="jless"
 cd $WORKSPACE
 git clone https://github.com/PaulJuliusMartinez/jless.git
-cd jless
+cd ${pkgs}
 RUSTFLAGS="-C target-feature=+crt-static -C link-args=-L/usr/lib -lxcb -lXau -lXdmcp -C linker=clang -C strip=symbols -C opt-level=s"
 cargo build --bin jless --target ${HOST_ARCH}-chimera-linux-musl --release
 cd ./target/${HOST_ARCH}-chimera-linux-musl/release/
-XZ_OPT=-e9 tar vcJf ./jless.tar.xz jless
-mv ./jless.tar.xz /work/artifact/
+XZ_OPT=-e9 tar vcJf ./${pkgs}.tar.xz -C /work/artifact/ ${pkgs}
 
 # amp
+pkgs="amp"
 cd $WORKSPACE
 git clone https://github.com/jmacdonald/amp.git
-cd amp
+cd ${pkgs}
 RUSTFLAGS="-C target-feature=+crt-static -C link-args=-L/usr/lib -lxcb -lXau -lXdmcp -C linker=clang -C strip=symbols -C opt-level=s"
 cargo build --target ${HOST_ARCH}-chimera-linux-musl --release
 cd ./target/${HOST_ARCH}-chimera-linux-musl/release/
-XZ_OPT=-e9 tar vcJf ./amp.tar.xz amp
-mv ./amp.tar.xz /work/artifact/
+XZ_OPT=-e9 tar vcJf ./${pkgs}.tar.xz -C /work/artifact/ ${pkgs}
 
 # starship
+pkgs="starship"
 cd $WORKSPACE
 git clone https://github.com/starship/starship.git
-cd starship
+cd ${pkgs}
 RUSTFLAGS="-C target-feature=+crt-static -C linker=clang -C strip=symbols -C opt-level=s"
 cargo build --target ${HOST_ARCH}-chimera-linux-musl --release
 cd ./target/${HOST_ARCH}-chimera-linux-musl/release/
-XZ_OPT=-e9 tar vcJf ./starship.tar.xz starship
-mv ./starship.tar.xz /work/artifact/
+XZ_OPT=-e9 tar vcJf ./${pkgs}.tar.xz -C /work/artifact/ ${pkgs}
 
 # eza
+pkgs="eza"
 cd $WORKSPACE
 git clone https://github.com/eza-community/eza.git
-cd eza
+cd ${pkgs}
 RUSTFLAGS="-C target-feature=+crt-static -C linker=clang -C strip=symbols -C opt-level=s"
 cargo build --target ${HOST_ARCH}-chimera-linux-musl --release
 cd ./target/${HOST_ARCH}-chimera-linux-musl/release/
-XZ_OPT=-e9 tar vcJf ./eza.tar.xz eza
-mv ./eza.tar.xz /work/artifact/
+XZ_OPT=-e9 tar vcJf ./${pkgs}.tar.xz -C /work/artifact/ ${pkgs}
