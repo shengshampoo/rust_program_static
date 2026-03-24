e60218b953c4af3342a481a996d08d95495c237b
shengshampoo <shengzh@tutanota.com>
2026-03-24 13:26:44 +0000

    Update build-termux-rust.sh

--- build-termux-rust.sh
+++ build-termux-rust.sh
@@ -12,19 +12,18 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --profile m
 source $HOME/.cargo/env && rustup target add aarch64-linux-android
 
 # https://github.com/HomuHomu833/android-ndk-custom
-#curl -sL "https://github.com/HomuHomu833/android-ndk-custom/releases/download/r29/android-ndk-r29-$(uname -m)-linux-musl.tar.xz" | tar x --xz
-curl -LO https://dl.google.com/android/repository/android-ndk-r29-linux.zip && unzip android-ndk-r29-linux.zip
+curl -sL "https://github.com/HomuHomu833/android-ndk-custom/releases/download/r29/android-ndk-r29-$(uname -m)-linux-musl.tar.xz" | tar x --xz
 
 mv /usr/bin/cc /usr/bin/cc.old
-ln -sf /android-ndk-r29/toolchains/llvm/prebuilt/linux-$(uname -m)/bin/clang /usr/bin/cc
-export CC="/android-ndk-r29/toolchains/llvm/prebuilt/linux-$(uname -m)/bin/aarch64-linux-android23-clang"
-export CXX="/android-ndk-r29/toolchains/llvm/prebuilt/linux-$(uname -m)/bin/aarch64-linux-android23-clang++"
-export AR="/android-ndk-r29/toolchains/llvm/prebuilt/linux-$(uname -m)/bin/llvm-ar"
+ln -sf /android-ndk-r29/toolchains/llvm/prebuilt/linux-x86_64/bin/clang /usr/bin/cc
+export CC="/android-ndk-r29/toolchains/llvm/prebuilt/linux-linux-x86_64/bin/aarch64-linux-android23-clang"
+export CXX="/android-ndk-r29/toolchains/llvm/prebuilt/linux-linux-x86_64/bin/aarch64-linux-android23-clang++"
+export AR="/android-ndk-r29/toolchains/llvm/prebuilt/linux-linux-x86_64/bin/llvm-ar"
 export AS=$CC
-export LD="/android-ndk-r29/toolchains/llvm/prebuilt/linux-$(uname -m)/bin/ld"
-export RANLIB="/android-ndk-r29/toolchains/llvm/prebuilt/linux-$(uname -m)/bin/llvm-ranlib"
-export STRIP="/android-ndk-r29/toolchains/llvm/prebuilt/linux-$(uname -m)/bin/llvm-strip"
-export PATH=/android-ndk-r29/toolchains/llvm/prebuilt/linux-$(uname -m)/bin/:$PATH
+export LD="/android-ndk-r29/toolchains/llvm/prebuilt/linux-x86_64/bin/ld"
+export RANLIB="/android-ndk-r29/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ranlib"
+export STRIP="/android-ndk-r29/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-strip"
+export PATH=/android-ndk-r29/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH
 export ANDROID_NDK_HOME="/android-ndk-r29"
 export ANDROID_NDK="/android-ndk-r29"
 export ANDROID_NDK_ROOT="/android-ndk-r29"
