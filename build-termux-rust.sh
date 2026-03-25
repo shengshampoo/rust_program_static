7d390809249e35aeb473fe57e96c9f3f51005eed
shengshampoo <shengzh@tutanota.com>
2026-03-25 02:17:11 +0000

    Update build-termux-rust.sh

--- build-termux-rust.sh
+++ build-termux-rust.sh
@@ -11,7 +11,8 @@ mkdir -p /work/artifact
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --profile minimal --default-toolchain stable -y
 source $HOME/.cargo/env && rustup target add aarch64-linux-android
 
-curl -LO https://dl.google.com/android/repository/android-ndk-r29-linux.zip && unzip android-ndk-r29-linux.zip
+# https://github.com/HomuHomu833/android-ndk-custom
+curl -sL "https://github.com/HomuHomu833/android-ndk-custom/releases/download/r29/android-ndk-r29-$(uname -m)-android.tar.xz " | tar x --xz
 
 mv /usr/bin/cc /usr/bin/cc.old
 ln -sf /android-ndk-r29/toolchains/llvm/prebuilt/linux-x86_64/bin/clang /usr/bin/cc
