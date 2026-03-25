d962b452f71241380967af58c34fdea58e5aad9c
shengshampoo <shengzh@tutanota.com>
2026-03-25 02:19:18 +0000

    Update build-termux-rust.sh

--- build-termux-rust.sh
+++ build-termux-rust.sh
@@ -12,7 +12,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --profile m
 source $HOME/.cargo/env && rustup target add aarch64-linux-android
 
 # https://github.com/HomuHomu833/android-ndk-custom
-curl -sL "https://github.com/HomuHomu833/android-ndk-custom/releases/download/r29/android-ndk-r29-$(uname -m)-android.tar.xz " | tar x --xz
+curl -sL "https://github.com/HomuHomu833/android-ndk-custom/releases/download/r29/android-ndk-r29-$(uname -m)-linux-android.tar.xz" | tar x --xz
 
 mv /usr/bin/cc /usr/bin/cc.old
 ln -sf /android-ndk-r29/toolchains/llvm/prebuilt/linux-x86_64/bin/clang /usr/bin/cc
