96c2ae8def107080a5d1fb79328e79bc37a6734d
shengshampoo <shengzh@tutanota.com>
2026-03-31 11:32:25 +0000

    Update build-termux-rust.sh

--- build-termux-rust.sh
+++ build-termux-rust.sh
@@ -11,8 +11,7 @@ mkdir -p /work/artifact
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --profile minimal --default-toolchain stable -y
 source $HOME/.cargo/env && rustup target add aarch64-linux-android
 
-cd /
-curl -sL "https://github.com/HomuHomu833/android-ndk-custom/releases/download/r29/android-ndk-r29-$(uname -m)-linux-musl.tar.xz" | tar x --xz
+curl -LO https://dl.google.com/android/repository/android-ndk-r29-linux.zip && unzip android-ndk-r29-linux.zip
 
 mv /usr/bin/cc /usr/bin/cc.old
 ln -sf /android-ndk-r29/toolchains/llvm/prebuilt/linux-x86_64/bin/clang /usr/bin/cc
@@ -32,7 +31,7 @@ export ANDROID_NDK_ROOT="/android-ndk-r29"
 cd $WORKSPACE
 git clone https://github.com/sigoden/dufs
 cd dufs
-RUSTFLAGS="-C target-feature=+crt-static -C linker=/android-ndk-r29/toolchains/llvm/prebuilt/linux-86_64/bin/aarch64-linux-android23-clang -C strip=symbols -C opt-level=s" cargo build --target aarch64-linux-android --release
+RUSTFLAGS="-C target-feature=+crt-static -C linker=/android-ndk-r29/toolchains/llvm/prebuilt/linux-$(uname -m)/bin/aarch64-linux-android23-clang -C strip=symbols -C opt-level=s" cargo build --target aarch64-linux-android --release
 cd ./target/aarch64-linux-android/release/
 tar vcJf ./dufs.tar.xz dufs
 mv ./dufs.tar.xz /work/artifact/
